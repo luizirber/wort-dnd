aa65d205623ef185f2d157985bd37bd8d3cd09ae
Luiz Irber <luiz.irber@gmail.com>
2018-07-10 17:26:55 +0000

    autogen index

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,13 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
 module.exports = {
   entry: "./index.js",
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'wort DnD'
+    })
+  ],
   output: {
     path: path.resolve(__dirname, "dist"),
     filename: "index.js",
