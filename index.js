f7a103bc9b40325cb2f58c5305ff6f0733eb2b42
Luiz Irber <luiz.irber@gmail.com>
2018-08-17 16:23:25 -0700

    up

--- index.js
+++ index.js
@@ -8,55 +8,55 @@ const js = import("sourmash/sourmash.js");
 
 const IPFS = require('ipfs')
 
-js.then(Sourmash => {
-  var app = choo()
+var app = choo()
 
-  app.use(function (state, emitter) {
-    state.fileNames = []
+app.use(function (state, emitter) {
+  state.fileNames = []
+  js.then(Sourmash => {
     state.Sourmash = Sourmash
-    state.ready = false
+  })
+  state.ready = false
 
-    const options = {
-      repo: 'ipfs-' + Math.random(),
-      config: {
-        Addresses: {
-          Swarm: ['/dns4/ws-star.discovery.libp2p.io/tcp/443/wss/p2p-websocket-star']
-        }
+  const options = {
+    repo: 'ipfs-' + Math.random(),
+    config: {
+      Addresses: {
+        Swarm: ['/dns4/ws-star.discovery.libp2p.io/tcp/443/wss/p2p-websocket-star']
       }
     }
+  }
 
-    var node = new IPFS(options)
+  var node = new IPFS(options)
 
-    node.once('start', () => {
-      node.id()
-        .then((id) => {
-          state.info = id
-          emitter.emit("IPFSReady")
-          //updateView('ready', node)
-          //onSuccess('Node is ready.')
-          //setInterval(refreshPeerList, 1000)
-          //setInterval(sendFileList, 10000)
-        })
-        .catch((error) => alert(error))
-    })
-
-    state.node = node
-    state.Buffer = node.types.Buffer
-
-    emitter.on('fileDrop', function (data) {
-      Array.prototype.push.apply(state.fileNames, data);
-      emitter.emit('render')
-    })
-
-    emitter.on('IPFSReady', function () {
-      state.ready = true
-      emitter.emit('render')
-    })
+  node.once('start', () => {
+    node.id()
+      .then((id) => {
+        state.info = id
+        emitter.emit("IPFSReady")
+        //updateView('ready', node)
+        //onSuccess('Node is ready.')
+        //setInterval(refreshPeerList, 1000)
+        //setInterval(sendFileList, 10000)
+      })
+      .catch((error) => alert(error))
   })
 
-  app.route('/', require('./templates/main'))
-  app.route('/*', require('./templates/404'))
+  state.node = node
+  state.Buffer = node.types.Buffer
 
-  if (!module.parent) app.mount('body')
-  else module.exports = app
-});
+  emitter.on('fileDrop', function (data) {
+    Array.prototype.push.apply(state.fileNames, data);
+    emitter.emit('render')
+  })
+
+  emitter.on('IPFSReady', function () {
+    state.ready = true
+    emitter.emit('render')
+  })
+})
+
+app.route('/', require('./templates/main'))
+app.route('/*', require('./templates/404'))
+
+if (!module.parent) app.mount('body')
+else module.exports = app
